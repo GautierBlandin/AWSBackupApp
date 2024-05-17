980500eedeafc10896199a76ca6637339ee8d131
Gautier <gautier.blandin@gmail.com>
2024-05-17 23:52:18 +0200

    feat(AB-11): add di container lib

--- lib/di-container/Provider.ts
+++ lib/di-container/Provider.ts
@@ -14,10 +14,8 @@ export type ClassProvider<T> = {
   useClass: new () => T;
 };
 
-export const isValueProvider = <T>(provider: Provider<T>):
-  provider is ValueProvider<T> => (provider as ValueProvider<T>).useValue !== undefined;
+export const isValueProvider = <T>(provider: Provider<T>): provider is ValueProvider<T> => (provider as ValueProvider<T>).useValue !== undefined;
 export const isFactoryProvider = <T>(provider: Provider<T>): provider is FactoryProvider<T> => (
   (provider as FactoryProvider<T>).useFactory !== undefined
 );
-export const isClassProvider = <T>(provider: Provider<T>)
-  : provider is ClassProvider<T> => (provider as ClassProvider<T>).useClass !== undefined;
+export const isClassProvider = <T>(provider: Provider<T>): provider is ClassProvider<T> => (provider as ClassProvider<T>).useClass !== undefined;
