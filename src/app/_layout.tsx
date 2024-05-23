e3fad4db9c00c5104fcd0a92f7e3cd153240b3ba
Gautier <gautier.blandin@gmail.com>
2024-05-23 23:38:04 +0200

    feat(AB-9): load tamagui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,10 +1,22 @@
 import { Stack } from 'expo-router/stack';
 import { registerInfrastructure } from '@/compositionRoot';
 import { TamaguiProvider } from 'tamagui';
+import { useFonts } from 'expo-font';
 import tamaguiConfig from '../../tamagui.config';
 
 export default function AppLayout() {
   registerInfrastructure();
+
+  const [loaded] = useFonts({
+    // eslint-disable-next-line global-require
+    Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+    // eslint-disable-next-line global-require
+    InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+  });
+
+  if (!loaded) {
+    return null;
+  }
 
   return (
     <TamaguiProvider config={tamaguiConfig}>
