ff0cbb0c9892fc2985d570790a26a1a86acd10e4
Gautier <gautier.blandin@gmail.com>
2024-05-23 23:51:38 +0200

    feat(AB-9): prettier settings UI!

--- src/app/(tabs)/settings.tsx
+++ src/app/(tabs)/settings.tsx
@@ -1,7 +1,33 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { View, TextInput, Button } from 'react-native';
+
 import { SettingsUseCase } from '@/useCases/settings';
 import crossPlatformAlert from '@/components/CrossPlatformAlert';
+import {
+  Button, Input, Text, View, YStack,
+} from 'tamagui';
+
+function FormInput({
+  label, value, onChangeText, secureTextEntry,
+}: {
+  label: string,
+  value: string,
+  onChangeText: (text: string) => void,
+  secureTextEntry?: boolean,
+}) {
+  return (
+    <View>
+      <YStack>
+        <Text fontSize={12} padding={2} marginLeft={4}>{label}</Text>
+        <Input
+          placeholder={label}
+          value={value}
+          onChangeText={onChangeText}
+          secureTextEntry={secureTextEntry}
+        />
+      </YStack>
+    </View>
+  );
+}
 
 function Settings() {
   const [accessKey, setAccessKey] = useState('');
@@ -35,28 +61,35 @@ function Settings() {
 
   return (
     <View>
-      <TextInput
-        placeholder="Access Key"
-        value={accessKey}
-        onChangeText={setAccessKey}
-      />
-      <TextInput
-        placeholder="Secret Access Key"
-        value={secretAccessKey}
-        onChangeText={setSecretAccessKey}
-        secureTextEntry
-      />
-      <TextInput
-        placeholder="Region"
-        value={region}
-        onChangeText={setRegion}
-      />
-      <TextInput
-        placeholder="Bucket Name"
-        value={bucketName}
-        onChangeText={setBucketName}
-      />
-      <Button title="Save Credentials" onPress={handleSaveCredentials} />
+      <YStack margin={16} gap={16}>
+        <FormInput
+          label="Access Key"
+          value={accessKey}
+          onChangeText={setAccessKey}
+        />
+        <FormInput
+          label="Secret Access Key"
+          value={secretAccessKey}
+          onChangeText={setSecretAccessKey}
+          secureTextEntry
+        />
+        <FormInput
+          label="Region"
+          value={region}
+          onChangeText={setRegion}
+        />
+        <FormInput
+          label="Bucket Name"
+          value={bucketName}
+          onChangeText={setBucketName}
+        />
+        <Button
+          backgroundColor="blue"
+          onPress={handleSaveCredentials}
+        >
+          <Text color="whitesmoke">Save Credentials</Text>
+        </Button>
+      </YStack>
     </View>
   );
 }
