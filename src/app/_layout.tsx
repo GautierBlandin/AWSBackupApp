556b8d6afe358bbca70bc2e687f8ada28695e9d8
Gautier <gautier.blandin@gmail.com>
2024-05-23 22:25:16 +0200

    feat(AB-9): install tamagui

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,12 +1,16 @@
 import { Stack } from 'expo-router/stack';
 import { registerInfrastructure } from '@/compositionRoot';
+import { TamaguiProvider } from 'tamagui';
+import tamaguiConfig from '../../tamagui.config';
 
 export default function AppLayout() {
   registerInfrastructure();
 
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-    </Stack>
+    <TamaguiProvider config={tamaguiConfig}>
+      <Stack>
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      </Stack>
+    </TamaguiProvider>
   );
 }
