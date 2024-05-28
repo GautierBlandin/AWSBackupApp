6dbad9b108ce693560af6f1c8781f11718a40bd8
Gautier <gautier.blandin@gmail.com>
2024-05-28 10:04:32 +0200

    feat(AB-18): remove bad src dir

--- src/ports/MediaLibrary.mock.ts
+++
@@ -1,5 +0,0 @@
-export const mockMediaLibraryFactory = () => ({
-  getAssetsAsync: jest.fn(),
-  getPermissionsAsync: jest.fn(),
-  requestPermissionsAsync: jest.fn(),
-});
--- src/ports/MediaLibraryToken.ts
+++
@@ -1,4 +0,0 @@
-import { createInjectionToken } from '@ab/di-container';
-import { MediaLibrary } from '@/ports/MediaLibrary';
-
-export const mediaLibraryToken = createInjectionToken<MediaLibrary>('MediaLibrary');
